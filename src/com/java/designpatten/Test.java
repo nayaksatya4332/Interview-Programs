1d3b0549a50dd49bc46ecccc31454e85401e5538
nayaksatya4332 <135519573+nayaksatya4332@users.noreply.github.com>
2023-08-19 14:12:38 +0530

    Update Test.java

--- src/com/java/designpatten/Test.java
+++ src/com/java/designpatten/Test.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 public class Test {
 	public static void main(String[] args) {
+		//Testing Builder Design pattern
 		Student std = Student.builder()
 				.id(100)
 				.age(25)
